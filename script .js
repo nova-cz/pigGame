8e5bd7699019b50109c07743be806761afe1f28a
Miguel Angel Cuz Reyes <125812157+nova-cz@users.noreply.github.com>
2024-03-10 16:02:37 -0600

    updating JS file

--- script .js
+++ script .js
@@ -1,6 +1,8 @@
 'use strict'
 
 //Selecting elements
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
 const current0EL = document.getElementById('current--0');
@@ -16,7 +18,9 @@ score1EL.textContent = 0;
 //? -> Add a class to a element
 diceEL.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', ()=> {
@@ -33,11 +37,14 @@ btnRoll.addEventListener('click', ()=> {
     if(dice !== 1){
     //Add dice to current score
         currentScore += dice;
-        current0EL.textContent = currentScore;
-    
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     }else{
         //Switch to next player
-
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0EL.classList.toggle('player--active');
+        player1EL.classList.toggle('player--active');
     }
 });
 
